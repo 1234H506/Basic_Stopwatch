b2af18d4e21aa005c30c3db52a0df807e2a2133e
Hugo Dallabeneta <dallabenetahugo@gmail.com>
2026-08-20 20:00:34 +0100

    feat: Add Timer template and no administrator registration

--- startapp/timer/models.py
+++ startapp/timer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Timer(models.Model):
+
+    class Status(models.TextChoices):
+        EM_ANDAMENTO = 'em_andamento', 'Em andamento'
+        INTERROMPIDO = 'interrompido', 'Interrompido'
+        CONCLUIDO = 'concluido', 'Concluído'
+
+    start = models.DateTimeField()
+    vl_pause = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.EM_ANDAMENTO)
