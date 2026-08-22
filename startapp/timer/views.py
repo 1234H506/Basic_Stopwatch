25c7a3c7ccbd9bdd62ee62c16f4646c38e9c3d48
Hugo Dallabeneta <dallabenetahugo@gmail.com>
2026-08-22 20:19:35 +0100

    feat: Add timer start endpoint

--- startapp/timer/views.py
+++ startapp/timer/views.py
@@ -1,6 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import Timer
+from django.utils import timezone
 
 
 # Create your views here.
 def show_index(request):
     return render(request, 'timer/main.html')
+
+def start(request):
+    if request.method == 'POST':
+        timer = Timer.objects.create(start=timezone.now())
+        return JsonResponse({'id':timer.id, 'start': timer.start })
+    else:
+        return JsonResponse({'message': 'method not allowed'}, status=405)
